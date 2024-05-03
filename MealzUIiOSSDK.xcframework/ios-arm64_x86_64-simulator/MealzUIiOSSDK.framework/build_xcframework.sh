92bc78915ea23bf3f8c324954a3caefee2a7ffd0
diarmuidmcgonagle <diarmuidmcgonagle@gmail.com>
2024-05-03 18:51:25 +0200

    update bin

--- MealzUIiOSSDK.xcframework/ios-arm64_x86_64-simulator/MealzUIiOSSDK.framework/build_xcframework.sh
+++ MealzUIiOSSDK.xcframework/ios-arm64_x86_64-simulator/MealzUIiOSSDK.framework/build_xcframework.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-
+FRAMEWORK_NAME="MealzUIiOSSDK"
 BASE_DIR="${MEALZ_PROJECT_DIR:-/Users/diarmuidmcgonagle/sources/mealz/MealzUIiOSSDK}"
 
 # Define paths
 DERIVED_DATA_PATH="${DERIVED_DATA_DIR:-/Users/diarmuidmcgonagle/Library/Developer/Xcode/DerivedData/}"
-ARCHIVE_PATH_IOS="$BASE_DIR/build/MealzUIiOSSDK-ios.xcarchive"
-ARCHIVE_PATH_IOSSIM="$BASE_DIR/build/MealzUIiOSSDK-iossim.xcarchive"
-XCFRAMEWORK_OUTPUT_PATH="$BASE_DIR/build/MealzUIiOSSDK.xcframework"
+ARCHIVE_PATH_IOS="$BASE_DIR/build/$FRAMEWORK_NAME-ios.xcarchive"
+ARCHIVE_PATH_IOSSIM="$BASE_DIR/build/$FRAMEWORK_NAME-iossim.xcarchive"
+XCFRAMEWORK_OUTPUT_PATH="$BASE_DIR/build/$FRAMEWORK_NAME.xcframework"
 
 # Archive for iOS
-xcodebuild archive -scheme MealzUIiOSSDK -destination 'generic/platform=iOS' -archivePath "$ARCHIVE_PATH_IOS" -derivedDataPath "$DERIVED_DATA_PATH" SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive \
+-scheme "$FRAMEWORK_NAME" \
+-destination 'generic/platform=iOS' \
+-archivePath "$ARCHIVE_PATH_IOS" \
+-derivedDataPath "$DERIVED_DATA_PATH" \
+SKIP_INSTALL=NO \
+BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 # Check if the previous command was successful
 if [ $? -ne 0 ]; then
@@ -18,7 +24,13 @@ if [ $? -ne 0 ]; then
 fi
 
 # Archive for iOS Simulator
-xcodebuild archive -scheme MealzUIiOSSDK -destination 'generic/platform=iOS Simulator' -archivePath "$ARCHIVE_PATH_IOSSIM" -derivedDataPath "$DERIVED_DATA_PATH" SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive \
+-scheme "$FRAMEWORK_NAME" \
+-destination 'generic/platform=iOS Simulator' \
+-archivePath "$ARCHIVE_PATH_IOSSIM" \
+-derivedDataPath "$DERIVED_DATA_PATH" \
+SKIP_INSTALL=NO \
+BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 # Check if the previous command was successful
 if [ $? -ne 0 ]; then
@@ -34,10 +46,10 @@ fi
 
 # Create XCFramework
 xcodebuild -create-xcframework \
--framework "$ARCHIVE_PATH_IOS/Products/Library/Frameworks/MealzUIiOSSDK.framework" \
--debug-symbols "$ARCHIVE_PATH_IOS/dSYMs/MealzUIiOSSDK.framework.dSYM" \
--framework "$ARCHIVE_PATH_IOSSIM/Products/Library/Frameworks/MealzUIiOSSDK.framework" \
--debug-symbols "$ARCHIVE_PATH_IOSSIM/dSYMs/MealzUIiOSSDK.framework.dSYM" \
+-framework "$ARCHIVE_PATH_IOS/Products/Library/Frameworks/$FRAMEWORK_NAME.framework" \
+-debug-symbols "$ARCHIVE_PATH_IOS/dSYMs/$FRAMEWORK_NAME.framework.dSYM" \
+-framework "$ARCHIVE_PATH_IOSSIM/Products/Library/Frameworks/$FRAMEWORK_NAME.framework" \
+-debug-symbols "$ARCHIVE_PATH_IOSSIM/dSYMs/$FRAMEWORK_NAME.framework.dSYM" \
 -output "$XCFRAMEWORK_OUTPUT_PATH"
 
 # Check if the previous command was successful
