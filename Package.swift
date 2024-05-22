e7b06d2d77eec231a1ed077faee0454c4089bbc7
walerowicz-miam <damien.walerowicz@mealz.ai>
2024-05-22 07:34:25 +0200

    update release

--- Package.swift
+++ Package.swift
@@ -3,22 +3,31 @@
 
 import PackageDescription
 
+let configurationMode = "prod"
+
 let package = Package(
-    name: "MealzUIiOSSDKRelease",
+    name: "MealzUIiOSSDK",
     defaultLocalization: "fr",
     platforms: [
         .iOS(.v12),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MealzUIiOSSDKRelease",
-            targets: ["MealzUIiOSSDKRelease"]),
+            name: "MealzUIiOSSDK",
+            targets: ["MealzUIiOSSDK"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/miamtech/MealzIOSFrameworkRelease", from: "1.0.0")
     ],
     targets: [
         .binaryTarget(
-            name: "MealzUIiOSSDKRelease",
-            path: "./MealzUIiOSSDK.xcframework"
+            name: "MealzUIiOSSDK",
+            url: "https://github.com/Dwalero-org/MealzUIiOSSDKRelease/raw/release/0.0.3/MealzUIiOSSDK.zip",
+            checksum: "7e52caa9c85a8d8b187ffa290165513c911ed1c4dfd236799590e222656e3fd6"
         )
+        
     ]
 )
+/*
+ 
+ */
